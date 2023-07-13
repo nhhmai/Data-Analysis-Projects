61f0c7c3d85f91bf6164c62dfc56b2497a5e6fb3
nhhmai <102445990+nhhmai@users.noreply.github.com>
2023-07-12 18:48:15 -0700

    Add files via upload

---
+++ Student Exams.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE student_exams;
+
+CREATE TABLE student_info (
+	id INT(10),
+	gender VARCHAR(10),
+	ethnic_group VARCHAR(10),
+    parent_edu VARCHAR(20),
+    lunch_type VARCHAR(20),
+    test_prep VARCHAR(20),
+    parent_marital_status VARCHAR(10),
+    practice_sports VARCHAR(20),
+    first_child VARCHAR(10),
+    siblings INT(2),
+    means_of_transport VARCHAR(15),
+    weekly_study_hrs VARCHAR(8),
+    math_score INT(3),
+    reading_score INT(3),
+    writing_score INT(3)
+    );
+    
+
+    
